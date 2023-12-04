e75170e37dd0b85c19eb995f5dbf74d930fa5ed2
Faraji Ombonya <farajiombonya@gmail.com>
2023-12-04 15:33:22 +0300

    feat: the contact route ui

--- learning-react-router/src/main.jsx
+++ learning-react-router/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root.jsx'
 import ErrorPage from './error-page.jsx'
+import Contact from './routes/contact.jsx'
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
