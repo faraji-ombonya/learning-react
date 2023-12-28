e4734e2cc02d4f0145a956806dfcc5b7b07d00a9
Faraji Ombonya <farajiombonya@gmail.com>
2023-12-28 23:44:24 +0300

    feat: updating data

--- learning-react-router/src/main.jsx
+++ learning-react-router/src/main.jsx
@@ -8,6 +8,7 @@ import Root, {
  } from './routes/root.jsx'
 import ErrorPage from './error-page.jsx'
 import Contact, { loader as contactLoader} from './routes/contact.jsx'
+import EditContact from './routes/edit.jsx'
 
 
 const router = createBrowserRouter([
@@ -23,6 +24,11 @@ const router = createBrowserRouter([
         element: <Contact />,
         loader: contactLoader,
       },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact/>,
+        loader: contactLoader,
+      }
     ],
   },
 ]);
