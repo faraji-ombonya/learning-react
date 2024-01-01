4b4a143baa966b0a10381f73a7cfd46a17fa6396
Faraji Ombonya <farajiombonya@gmail.com>
2024-01-01 12:06:10 +0300

    feat: cancel button

--- learning-react-router/src/routes/edit.jsx
+++ learning-react-router/src/routes/edit.jsx
@@ -1,4 +1,9 @@
-import { Form, useLoaderData, redirect } from "react-router-dom";
+import { 
+  Form, 
+  useLoaderData, 
+  redirect,
+  useNavigate,
+} from "react-router-dom";
 import { updateContact } from "../contacts";
 
 
@@ -11,6 +16,7 @@ export async function action ({ request, params }){
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -60,7 +66,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+          onClick={()=>{ navigate(-1)}}
+          type="button">Cancel</button>
       </p>
     </Form>
   );
